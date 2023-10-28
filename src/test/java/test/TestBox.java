06a803a2f720843936ec2e8d7aceaf9547f93293
Юлия Вилкова <al.menskaya@gmail.com>
2023-10-28 16:06:42 +0200

    changed testox

--- src/test/java/test/TestBox.java
+++ src/test/java/test/TestBox.java
@@ -4,19 +4,15 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
-
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.executeJavaScript;
-
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.text;
 
 public class TestBox {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1920x1080";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
+        //  Configuration.holdBrowserOpen = true;
         Configuration.pageLoadStrategy = "eager";
     }
 
@@ -33,11 +29,12 @@ public class TestBox {
         $(".react-datepicker__month-select").selectOption("August");
         $(".react-datepicker__year-select").selectOption("1988");
         $(".react-datepicker__day--002:not(.react-datepicker__day--outside-month)").click();
+        $("#hobbies-checkbox-1 + label").click();
         $("#hobbies-checkbox-3 + label").click();
-        $("#subjectsInput").setValue("Subject");
+        $("#subjectsInput").setValue("History").pressEnter();
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
-        $("#uploadPicture").uploadFile(new File("src\\test\\java\\test\\imagesForTest.jfif"));
+        $("#uploadPicture").uploadFromClasspath("imagesForTest.jfif");
         $("#stateCity-label").scrollIntoView(true).click();
         $("#currentAddress").setValue("Belgrade");
         $("#state").click();
@@ -45,5 +42,19 @@ public class TestBox {
         $("#city").click();
         $("#react-select-4-option-1").click();
         $("#submit").scrollIntoView(true).click();
+
+        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        $("tbody tr:nth-child(1) td:last-child").shouldHave(text("Aleksandra Menskaia"));
+        $("tbody tr:nth-child(2) td:last-child").shouldHave(text("al.menskaya@gmail.com"));
+        $("tbody tr:nth-child(3) td:last-child").shouldHave(text("Female"));
+        $("tbody tr:nth-child(4) td:last-child").shouldHave(text("1234567890"));
+        $("tbody tr:nth-child(5) td:last-child").shouldHave(text("02 August,1988"));
+        $("tbody tr:nth-child(6) td:last-child").shouldHave(text("History"));
+        $("tbody tr:nth-child(7) td:last-child").shouldHave(text("Sports, Music"));
+        $("tbody tr:nth-child(8) td:last-child").shouldHave(text("imagesForTest.jfif"));
+        $("tbody tr:nth-child(9) td:last-child").shouldHave(text("Belgrade"));
+        $("tbody tr:nth-child(10) td:last-child").shouldHave(text("NCR Gurgaon"));
+
     }
+
 }
